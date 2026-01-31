605231017768755c4e88c07589f72d947d69038f
ReZorDoc <bul.strint64@gmail.com>
2026-01-31 13:41:00 +0300

    solve 66, 67, 69 problems

---
+++ src/easy/_67_Add_Binary/AddBinary.java
@@ -0,0 +1,50 @@
+package easy._67_Add_Binary;
+
+/*
+Given two binary strings a and b, return their sum as a binary string.
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+
+Example 2:
+Input: a = "1010", b = "1011"
+Output: "10101"
+
+Constraints:
+1 <= a.length, b.length <= 104
+a and b consist only of '0' or '1' characters.
+Each string does not contain leading zeros except for the zero itself.
+ */
+
+public class AddBinary {
+
+    public static void main(String[] args) {
+        System.out.println(addBinary("11", "10"));
+    }
+
+    public static String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry == 1) {
+            int sum = carry;
+            if (i >= 0) {
+                sum = sum + a.charAt(i) - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum = sum + b.charAt(j) - '0';
+                j--;
+            }
+            result.append(sum % 2);
+            carry = sum / 2;
+        }
+
+        return result.reverse().toString();
+    }
+
+}
